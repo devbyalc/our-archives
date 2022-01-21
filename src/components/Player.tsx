424930f32b6bdf4cdd4e9e79d2d98921a3891777
Alicia.Hadisaputra <Alicia.Hadisaputra@au.ey.com>
2022-01-22 09:57:57 +1100

    fixed buggy button

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,14 +2,9 @@ import './player.scss';
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
-
 function Player() {
 
     const [playing, setPlaying] = useState(false);
-
-    onclick = () => {
-        setPlaying(!playing);
-    }
 
     const pause = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-pause-circle-fill play" viewBox="0 0 16 16">
         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z" />
@@ -20,9 +15,9 @@ function Player() {
     </svg>)
 
     return (
-        <button type="button" onClick={() => onclick} id="play-button">
+        <button className="button" type="button" onClick={(event) => setPlaying(!playing)} id="play-button">
             {playing ? pause() : play()}
-            <ReactPlayer className="hide" playing={playing} url='https://soundcloud.com/margaretaalicia/growaswego?si=bf52724242c14de4a45896de0b57bf74&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing' />
+            <ReactPlayer className="hide" playing={playing} onEnded={()=>setPlaying(!playing)} url='https://soundcloud.com/margaretaalicia/growaswego?si=bf52724242c14de4a45896de0b57bf74&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing' />
         </button>
     );
 }
