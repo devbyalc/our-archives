5dbea0ba68484f405662b290f7ea9f42c92bf3cf
Alicia.Hadisaputra <Alicia.Hadisaputra@au.ey.com>
2022-01-21 20:13:49 +1100

    Added player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,30 @@
 import './player.scss';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+
 
 function Player() {
+
+    const [playing, setPlaying] = useState(false);
+    const audio = new Audio("../assets/grow-as-we-go.m4a");
+
+    onclick = () => {
+        setPlaying(!playing);
+        playing ? audio.play() : audio.pause();
+    }
+
+    const pause = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-pause-circle-fill play" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z" />
+    </svg>)
+
+    const play = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-play-circle-fill play" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+    </svg>)
+
     return (
-        <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-play-circle-fill play" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-            </svg>
+        <button onClick={() => onclick}>
+            {playing ? pause() : play()}
+            <ReactPlayer className="hide" playing={playing} url='https://www.youtube.com/watch?v=rN9mWjoeuig' />
         </button>
     );
 }
