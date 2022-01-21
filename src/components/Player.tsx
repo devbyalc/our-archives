dedbf434dd0316024ba1e7d3b461f1288f91f57c
Alicia.Hadisaputra <Alicia.Hadisaputra@au.ey.com>
2022-01-21 18:49:06 +1100

    player styling

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,9 @@
-import { Container } from 'react-bootstrap';
 import './player.scss';
 
 function Player() {
     return (
         <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-play-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-play-circle-fill play" viewBox="0 0 16 16">
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
             </svg>
         </button>
