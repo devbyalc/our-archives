1be58af9d7204b26520d637af9f248d04a774bb4
Alicia.Hadisaputra <Alicia.Hadisaputra@au.ey.com>
2022-01-22 11:02:36 +1100

    Added menu nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import './navbar.scss';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import { NavDropdown } from 'react-bootstrap';
 
 function Navbar() {
     return (
         <Container>
             <Nav className="justify-content-between">
                 <Nav.Item><Nav.Link><span className='logo'>devbyalc</span></Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link>menù</Nav.Link></Nav.Item>
+                {/* <Nav.Item><Nav.Link>menù</Nav.Link></Nav.Item> */}
+                <NavDropdown title="menù" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">On Special</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.2">Youtube</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Audio-only</NavDropdown.Item>
+               
+                </NavDropdown>
             </Nav>
         </Container>
 
