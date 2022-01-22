a25b154a0cc93f4693819b550cb9b85d00301b2b
Alicia.Hadisaputra <Alicia.Hadisaputra@au.ey.com>
2022-01-22 23:26:22 +1100

    added scrolldown

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,20 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Content from './components/Content';
 import { Stack } from 'react-bootstrap';
-import {useState} from'react';
+import { useState } from 'react';
 
 function App() {
   const [tabKey, setKey] = useState('1');
 
-
-  // tab navigation
-  const getKey = (key:any) => {
+  // Tab navigation
+  const getKey = (key: any) => {
     setKey(key);
-    document.getElementById("el")?.scrollIntoView()
   }
   return (
     <Stack>
       <Navbar getTabKey={getKey} />
-      <Header/>
-      <Content tab={tabKey} getTabKey={getKey}/>
-      <div id="dummy"></div>
+      <Header />
+      <Content tab={tabKey} getTabKey={getKey} />
     </Stack>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,24 +3,25 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { NavDropdown } from 'react-bootstrap';
 
-function Navbar(props:any) {
-    
-    const onTrigger = (num:string, e?:any) => {
+function Navbar(props: any) {
+
+    const onTrigger = (num: string, e?: any) => {
         props.getTabKey(num);
-      
+        document.getElementById('content')?.scrollIntoView();
     }
+
     return (
         <Container>
             <Nav className="justify-content-between">
                 <Nav.Item><Nav.Link><span className='logo'>devbyalc</span></Nav.Link></Nav.Item>
-                {/* <Nav.Item><Nav.Link>menù</Nav.Link></Nav.Item> */}
                 <NavDropdown title="menù" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1" onClick={(e)=> onTrigger("1",e)}>On Special</NavDropdown.Item>
+                    <button className="dropdown-item" onClick={(e) => onTrigger("1", e)}>On Special</button>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.2" onClick={(e)=> onTrigger("2",e)}>Youtube</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3" onClick={(e)=> onTrigger("3",e)}>Audio-only</NavDropdown.Item>
+                    <button className="dropdown-item" onClick={(e) => onTrigger("2", e)}>Youtube</button>
+                    <button className="dropdown-item" onClick={(e) => onTrigger("3", e)}>Audio-only</button>
                 </NavDropdown>
             </Nav>
+            
         </Container>
 
     );
